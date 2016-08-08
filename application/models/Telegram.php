bb1a04bc57afa454f7797418caedb6fffe725958
duhow <contacto@duhowpi.net>
2016-08-09 00:19:45 +0200

    Fix de mal cerrado.

--- application/models/Telegram.php
+++ application/models/Telegram.php
@@ -405,7 +405,7 @@ class Telegram extends CI_Model{
 		return FALSE;
 	}
 
-	function is_bot($user = NULL){ if($user === NULL){ $user = $this->user->username; } return (!empty($user) && substr(strtolower($user), -3) == "bot") }
+	function is_bot($user = NULL){ if($user === NULL){ $user = $this->user->username; } return (!empty($user) && substr(strtolower($user), -3) == "bot"); }
 
     function dump($json = FALSE){ return($json ? json_encode($this->data) : $this->data); }
 
