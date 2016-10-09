26b571c4f5acadbac0a82b74fca0f4fd64059ef1
duhow <contacto@duhowpi.net>
2016-10-09 14:45:00 +0200

    FIX Link del grupo (no markdown).

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1608,7 +1608,7 @@ class Main extends CI_Controller {
 				$telegram->send
 					->notification(TRUE)
 					->chat($telegram->user->id)
-					->text($text, TRUE)
+					->text($text, NULL) // TODO NO Markdown.
 				->send();
 
 				if($pokeuser->verified){
