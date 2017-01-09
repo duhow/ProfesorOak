c92f79b069607909bdfbdb6034e1aef0b47a42b8
duhow <contacto@duhowpi.net>
2017-01-09 22:02:26 +0100

    Juego de puntos, añadido nivel / EXP.

--- application/plugins/game_expchat.php
+++ application/plugins/game_expchat.php
@@ -1,13 +1,45 @@
 <?php
 
+$levels = [
+	0,
+	200,
+	500,
+	1000,
+	2000,
+	4000,
+	7500,
+	12500,
+	25000,
+	50000,
+];
+
+function expchat_level($points){
+	for($i = 0; $i < count($levels); $i++){
+		if($points > $levels[$i]){ continue; }
+		return $i;
+	}
+}
+
 if($telegram->is_chat_group()){
 	if($telegram->words() == mt_rand(2, 9)){
 		$timeout = $pokemon->settings($telegram->user->id, 'expchat_timeout');
 		if(empty($timeout) or time() >= $timeout){
 			$points = $pokemon->settings($telegram->user->id, 'expchat_points');
+
+			$curlev = expchat_level($points);
+			$newpoints = $points + $telegram->words();
+
+			$nextlev = expchat_level($newpoints);
+
+			if($nextlev > $curlev){
+				$telegram->send
+					->notification(FALSE)
+					->text($telegram->user->first_name ." ha subido al *nivel " .$nextlev ."*!", TRUE)
+				->send();
+			}
 			// La recompensa será el número de palabras que haya tocado, para hacer el factor diferencial.
 			// Puede favorecer a los spamers, así que cuidado.
-			$pokemon->settings($telegram->user->id, 'expchat_points', $points + $telegram->words());
+			$pokemon->settings($telegram->user->id, 'expchat_points', $newpoints);
 			$pokemon->settings($telegram->user->id, 'expchat_timeout', time() + 60);
 		}
 	}
@@ -16,9 +48,9 @@ if($telegram->is_chat_group()){
 if($telegram->text_has("mi experiencia")){
 	$points = $pokemon->settings($telegram->user->id, 'expchat_points');
 	if(empty($points)){ $points = 0; }
-
+	$level = expchat_level($points);
 	$telegram->send
-		->text("Tienes $points puntos.")
+		->text("L" .$points ." / $points EXP")
 	->send();
 	return -1;
 }
