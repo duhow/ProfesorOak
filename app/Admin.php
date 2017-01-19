7adaff3e9b23cf2f045b5826236155b7551c7a90
duhow <contacto@duhowpi.net>
2017-01-19 14:50:57 +0100

    More updates.

--- app/Admin.php
+++ app/Admin.php
@@ -1,13 +1,37 @@
 <?php
 
 class Admin extends TelegramApp\Module {
-
-	public function __construct(){
-		  parent::__construct();
-	}
+	protected $runCommands = FALSE;
 
 	public function hooks(){
 
+	}
+
+	public function antipalmera(){
+		if($this->telegram->is_chat_group() && $this->telegram->sticker()){
+			$palmeras = [
+		        'BQADBAADzw0AAu7oRgABumTXtan23SUC',
+		        'BQADBAAD0Q0AAu7oRgABXE1L0Qpaf_sC',
+		        'BQADBAAD0w0AAu7oRgABq22PAgABeiCcAg',
+		        // GATO
+		        'BQADBAAD4wEAAqKYZgABGO27mNGhdSUC',
+		        'BQADBAAD5QEAAqKYZgABe9jp1bTT8jcC',
+		        'BQADBAAD5wEAAqKYZgABiX1O201m5X0C',
+		    ];
+			if(in_array($this->telegram->sticker(), $palmeras)){
+		        // $admins = array();
+		        /* if(function_exists('telegram_admins')){
+		            $admins = telegram_admins(TRUE);
+		            if(in_array($this->config->item('telegram_bot_id'), $admins)){
+		                if(in_array($telegram->user->id, $admins)){ return; }
+		                $telegram->send->text("¡¡PALMERAS NO!!")->send();
+		                $telegram->send->kick($telegram->user->id, $telegram->chat->id);
+		            }
+		        } */
+		        return TRUE;
+		    }
+		}
+		return FALSE;
 	}
 
 	public function kick($user, $chat){
--- app/Cleverbot.php
+++ app/Cleverbot.php
@@ -1,12 +1,32 @@
 <?php
 
 class Cleverbot extends TelegramApp\Module {
+	protected $runCommands = FALSE;
+	private $chatter = NULL;
 
 	public function __construct(){
-		  parent::__construct();
+		$file = 'libs/chatter-bot-api/php/chatterbotapi.php';
+		if(file_exists($file)){
+			require_once $file;
+			$factory = new ChatterBotFactory();
+			$bot1 = $factory->create(1); // Cleverbot
+			$this->chatter = $bot1->createSession();
+		}
 	}
 
-	public function hooks(){
+	public function think($text){
+		if($this->chatter === NULL){ return FALSE; }
+		return $this->telegram->send
+			->notification(FALSE)
+			->text($text)
+		->send();
+	}
 
+	protected function hooks(){
+		if($this->telegram->text_has("Oye", ["Oak", "profe"], TRUE) && $this->telegram->words() > 2){
+			return $this->think($this->telegram->words(2, 20));
+		}elseif($this->telegram->text_command("cleverbot") && $this->telegram->words() > 1){
+			return $this->think($this->telegram->words(1, 20));
+		}
 	}
 }
