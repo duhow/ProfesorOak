8351ac79230c6a17cf83007aef11aa72b6fad194
duhow <contacto@duhowpi.net>
2017-01-23 22:46:07 +0100

    Some changes.

---
+++ app/GameDubs.php
@@ -0,0 +1,39 @@
+<?php
+
+class GameDubs extends TelegramApp\Module {
+	protected $runCommands = FALSE;
+
+	public function run(){
+		if(
+			$this->user->settings['dubs'] &&
+			$this->telegram->key == "message"
+		){
+			$nums = array_merge(
+				range(11111, 99999, 11111),
+				range(1111, 9999, 1111),
+				range(111, 999, 111)
+				// range(11, 99, 11)
+			);
+			$lon = NULL;
+			$id = $telegram->message;
+			foreach($nums as $n){
+				if(@strpos(strval($id), strval($n), strlen($id) - strlen($n)) !== FALSE){
+					// $telegram->send->text("hecho en $id con $n")->send();
+					$lon = strlen($n);
+					break;
+				}
+			}
+			$str = NULL;
+			// if($lon == 2){ $str = "Dubs! :D"; }
+			if($lon == 3){ $str = "Trips checked!"; }
+			elseif($lon == 4){ $str = "QUADS *GET*!"; }
+			elseif($lon == 5){ $str = "QUINTUPLE *GET! OMGGG!!*"; }
+			if($str){
+				$telegram->send
+					->reply_to(TRUE)
+					->text($str, TRUE)
+				->send();
+			}
+		}
+	}
+}
