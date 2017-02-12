d92672365010ea63c3dc265f9f8a2254b2db92da
duhow <contacto@duhowpi.net>
2017-02-12 18:23:40 +0100

    Agregar Autobús Titsa - Tenerife.

--- application/plugins/autobus.php
+++ application/plugins/autobus.php
@@ -39,8 +39,6 @@ function autobus_cordoba($codigo){
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, $url);
-    // curl_setopt($ch, CURLOPT_POST, TRUE);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $get = curl_exec($ch);
     curl_close ($ch);
@@ -57,11 +55,39 @@ function autobus_cordoba($codigo){
     return array();
 }
 
+// Titsa - Tenerife
+function autobus_titsa($codigo){
+	$url = "http://titsa.com/ajax/xGetInfoParada.php";
+	$data = ['id_parada' => $codigo];
+	$url = $url ."?" .http_build_query($data);
+
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $get = curl_exec($ch);
+    curl_close ($ch);
+
+    if(!empty($get)){
+        $json = json_decode($get, TRUE);
+		if($json['success'] == FALSE){ return array(); }
+		if(empty($json['lineas'])){ return array(); }
+		if($json['parada'] == FALSE){ return array(); }
+		$lineas = array();
+		foreach($json['lineas'] as $r){
+			$lineas[] = "Linea " .$r['id'] ." - en " .$r['tiempo'] ." min - " .$r['descripcion'];
+		}
+		return $lineas;
+    }
+    return array();
+}
+
 if(
 	$this->telegram->words() == 2 &&
 	(
 		$this->telegram->text_command("amb") or
-		$this->telegram->text_command("aucorsa")
+		$this->telegram->text_command("aucorsa") or
+		$this->telegram->text_command("titsa")
 	)
 ){
     $num = $this->telegram->last_word(TRUE);
@@ -80,6 +106,8 @@ if(
 		$paradas = autobus_barcelona($num);
 	}elseif($this->telegram->text_command("aucorsa")){
 		$paradas = autobus_cordoba($num);
+	}elseif($this->telegram->text_command("titsa")){
+		$paradas = autobus_titsa($num);
 	}
 
     $str = "No encuentro paradas.";
