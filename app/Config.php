fe008ea777ec12be02176e2f75fe9b6abcbe6d75
duhow <contacto@duhowpi.net>
2017-03-15 16:04:29 +0100

    WIP Config, getting settings.

--- app/Config.php
+++ app/Config.php
@@ -13,7 +13,14 @@ class Config extends TelegramApp\Module {
 			if($this->telegram->text_has(["private", "p"])){
 				$this->telegram->send->chat($this->user->id);
 			}
-			$this->get($this->telegram->words(1));
+			$target = NULL;
+			if($this->user->id == CREATOR){
+				// if($this->telegram->reply_target('forward'))
+			}
+			$v = $this->get($this->telegram->words(1), NULL);
+			$this->telegram->send
+				->text($v)
+			->send();
 			$this->end();
 		}elseif($this->telegram->text_command("set") && in_array($this->telegram->words(), [3,4])){
 
@@ -26,26 +33,36 @@ class Config extends TelegramApp\Module {
 	}
 
 	public function get($key, $chat = NULL){
-		$lchat = $this->chat;
-		if(!empty($chat)){ $lchat = new Chat($chat); }
-		$lchat->load();
+		if(empty($chat)){ $chat = $this->chat->id; }
 
-		$res = @$lchat->settings[$key];
-		if(is_array($res) or is_bool($res)){ $res = json_encode($res); }
-		if(in_array(strtolower($key), ["all", "*"]) && $this->user->id == CREATOR){
-			$res = "";
-			foreach($lchat->settings as $k => $v){
-				$v = $this->parse_value($v);
-				if(is_array($v) or is_bool($v)){ $v = json_encode($v); }
-
-				$res .= $k .": " .$v ."\n";
-			}
+		// Si pide todos
+		if(in_array(strtolower($key), ["all", "*"])){
+			// Y no es el creador, fuera.
+			if($this->user->id != CREATOR){ return NULL; }
+		}else{
+			// Si sólo pide uno
+			$this->db->where('type', $key);
 		}
 
-		return $this->telegram->send
-			->text($res)
-		->send();
-		// return loaded settings?
+		$res = $this->db
+			->where('chat', $chat)
+		->get('settings');
+
+		if($this->db->count == 0){ return NULL; }
+		elseif($this->db->count == 1){
+			$v = $this->parse_value($res[0]['value']);
+			if(is_array($v) or is_bool($v)){ $v = json_encode($v); }
+			return $v;
+		}
+
+		$str = "";
+		foreach($res as $k => $v){
+			$v = $this->parse_value($v);
+			if(is_array($v) or is_bool($v)){ $v = json_encode($v); }
+
+			$str .= $k .": " .$v ."\n";
+		}
+		return $str;
 	}
 
 	public function parse_value($value){
