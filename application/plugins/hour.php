c2c441e0593f22b416aa4d9602cbb961c332bf45
duhow <duhowpi@gmail.com>
2017-03-30 19:22:21 +0200

    Mejora y arreglo en sistema de hora.

--- application/plugins/hour.php
+++ application/plugins/hour.php
@@ -1,6 +1,6 @@
 <?php
 
-if($this->telegram->text_has("Hora", TRUE) && $telegram->words() == 2){
+if($this->telegram->text_has("Hora", TRUE) && in_array($telegram->words(), [2,3])){
     $zonas = ['PDT' => -7, 'PST' => -8, 'CET' => 1, 'UTC' => 0, 'GMT' => 0];
     $sel = NULL;
     foreach($zonas as $z => $t){
@@ -15,21 +15,34 @@ if($this->telegram->text_has("Hora", TRUE) && $telegram->words() == 2){
     }
 
     $sum = 0;
+    $word = strtoupper($this->telegram->words(1));
 
-    foreach($this->telegram->words(TRUE) as $n => $w){
-        if($n == 0){ continue; }
-        if(strpos($w, $zonas[$sel]) === 0){
-            if(strlen($w) > strlen($zonas[$sel]) && in_array(substr($w, 3, 1), ["-", "+"])){
-                $sum = substr($w, 3);
-                $sum = intval($sum);
-            }
+    if(strpos($word, $zonas[$sel]) === 0){
+        if(strlen($word) > strlen($zonas[$sel]) && in_array(substr($word, 3, 1), ["-", "+"])){
+            $sum = substr($word, 3);
+            $sum = intval($sum);
         }
     }
 
     $time = time() - 3600; // Madrid - Rome
-    $time = $time + (3600 * $zonas[$sel]) + (3600 * $sum);
 
-    $str = ":clock: " .date("H:i:s") ."\n"
+    if($this->telegram->words() == 3){
+        $timefrom = NULL;
+        if(function_exists('time_parse')){
+            $timefrom = time_parse($this->telegram->words(2));
+            if(!empty($timefrom) && isset($timefrom['hour'])){
+
+                $time = strtotime($timefrom['hour']);
+                $local = $time - (3600 * $zonas[$sel]) - (3600*$sum); // TODO Check
+            }
+        }
+    }else{
+        $time = $time + (3600 * $zonas[$sel]) + (3600 * $sum);
+        $local = time(); // Current
+    }
+
+
+    $str = ":clock: " .date("H:i:s", $local) ."\n"
             .":world: " .date("H:i:s", $time);
 
     $str = $this->telegram->emoji($str);
