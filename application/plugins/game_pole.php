6f415974d57974d7439f2f87ac4b21c498c94ffb
duhow <contacto@duhowpi.net>
2017-04-05 00:08:15 +0200

    Matar procesamiento de pole cuando acaba.

--- application/plugins/game_pole.php
+++ application/plugins/game_pole.php
@@ -23,7 +23,7 @@ if($telegram->text_has(["pole", "subpole", "bronce"], TRUE) or $telegram->text_c
             ( $telegram->text_has("subpole", TRUE) && is_numeric($pole[1]) && date($timer) == date($timer, $pole[1]) ) or
             ( $telegram->text_has("bronce", TRUE) &&  is_numeric($pole[2]) && date($timer) == date($timer, $pole[2]) )
         ){
-            return;  // Mismo dia? nope.
+            return -1;  // Mismo dia? nope.
         }
     }
     $pole_user = unserialize($pokemon->settings($telegram->chat->id, 'pole_user'));
@@ -44,7 +44,7 @@ if($telegram->text_has(["pole", "subpole", "bronce"], TRUE) or $telegram->text_c
             }
         }
     }elseif($telegram->text_has("subpole", TRUE) and date($timer) == date($timer, $pole[0]) and $pole_user[1] == NULL){
-        if(in_array($telegram->user->id, $pole_user)){ return; } // Si ya ha hecho pole, nope.
+        if(in_array($telegram->user->id, $pole_user)){ return -1; } // Si ya ha hecho pole, nope.
         $pole[1] = time();
         $pole_user[1] = $telegram->user->id;
         $action = "la *subpole*";
@@ -55,7 +55,7 @@ if($telegram->text_has(["pole", "subpole", "bronce"], TRUE) or $telegram->text_c
             }
         }
     }elseif($telegram->text_has("bronce", TRUE) and date($timer) == date($timer, $pole[0]) and $pole_user[1] != NULL and $pole_user[2] == NULL){
-        if(in_array($telegram->user->id, $pole_user)){ return; } // Si ya ha hecho sub/pole, nope.
+        if(in_array($telegram->user->id, $pole_user)){ return -1; } // Si ya ha hecho sub/pole, nope.
         $pole[2] = time();
         $pole_user[2] = $telegram->user->id;
         $action = "el *bronce*";
@@ -66,14 +66,14 @@ if($telegram->text_has(["pole", "subpole", "bronce"], TRUE) or $telegram->text_c
             }
         }
     }else{
-        return;
+        return -1;
     }
 
     $pokemon->settings($telegram->chat->id, 'pole_points', serialize($pole));
     $pokemon->settings($telegram->chat->id, 'pole_user', serialize($pole_user));
     $telegram->send->text($telegram->user->first_name ." ha hecho $action!", TRUE)->send();
     // $telegram->send->text("Lo siento " .$telegram->user->first_name .", pero hoy la *pole* es mía! :D", TRUE)->send();
-    return;
+    return -1;
 }
 
 if($telegram->text_command("polerank") or $telegram->text_has("!polerank")){
