d67d9e354599879690393793ba3e20fc80ec66a2
duhow <contacto@duhowpi.net>
2017-06-05 11:16:13 +0200

    Re-FIX #103: Forzar mensaje de bienvenida.

--- application/plugins/base_newuser.php
+++ application/plugins/base_newuser.php
@@ -186,7 +186,7 @@ if($telegram->is_chat_group() && $telegram->data_received() == "new_chat_partici
 			// TODO Get avatar
 			// $text .= "Por favor, antes de nada, ponte una foto de avatar. La gente quiere ver caras familiares, no letras. :*" ."\n";
 		}
-        if(empty($pknew)){
+        if(empty($pknew) and $pokemon->settings($telegram->chat->id, 'force_welcome') != TRUE){
             $text .= "Oye, ¿podrías decirme el color de tu equipo?\n*Di: *_Soy ..._";
         }else{
             $emoji = ["Y" => "yellow", "B" => "blue", "R" => "red"];
