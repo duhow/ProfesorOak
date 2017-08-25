8ed513618f57be41acd07b981cf2725284d45add
duhow <contacto@duhowpi.net>
2017-08-25 10:56:51 +0200

    Limitar frase de crear raid para mensajes largos.

--- application/plugins/raid.php
+++ application/plugins/raid.php
@@ -15,7 +15,10 @@ if(!in_array(date("H"), range(8, 21))){
 
 if($this->telegram->is_chat_group() or $this->telegram->key == "channel_post"){
 
-	if($this->telegram->text_has(["montar", "monta", "crear", "organizar", "organiza"], ["raid", "incursión"])){
+	if(
+		$this->telegram->text_has(["montar", "monta", "crear", "organizar", "organiza"], ["raid", "incursión"]) and
+		$this->telegram->words() <= 20
+	){
 		$place = NULL;
 		if($this->telegram->text_has("en")){
 			$pos = strpos($this->telegram->text(), " en ") + strlen(" en ");
